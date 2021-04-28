8cc15728aad276d82f7b6f12600650975f23f42e
alvinche <alvinche@github.com>
2021-04-28 14:28:19 +0800

    lab lazy

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -46,9 +46,21 @@ sys_sbrk(void)
 
   if(argint(0, &n) < 0)
     return -1;
-  addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+//  addr = myproc()->sz;
+//  2021年04月27日16:56:01
+//  注释掉一下两行代码
+//  if(growproc(n) < 0)
+//    return -1;
+  struct proc* p = myproc();
+  addr = p->sz;
+  if (n < 0)
+  {
+    if (p->sz + n < 0) return -1;
+    else uvmdealloc(p->pagetable, p->sz, p->sz + n);
+  }
+  p->sz += n;
+
+//--------end-----------
   return addr;
 }
 
